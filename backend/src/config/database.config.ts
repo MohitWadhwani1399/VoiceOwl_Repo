b1fafc7d07652011d58065866b74a069a57d122a
MohitWadhwani1399 <mohitwadhwani1399@gmail.com>
2025-12-31 00:40:52 +0530

    commit mongodb config setup

--- backend/src/config/database.config.ts
+++ backend/src/config/database.config.ts
@@ -1,11 +1,24 @@
 import mongoose from "mongoose";
 
-const mongodb_uri = process.env.MONGODB_URI;
+export async function connectMongoDB(): Promise<void> {
+  const mongodb_uri = process.env.MONGODB_URI;
 
-if(!mongodb_uri){
+  if (!mongodb_uri) {
     throw new Error("MONGODB_URI is not defined");
+  }
+
+  mongoose.set("strictQuery", true);
+  // strictQuery is set to true to avoid searching fields which are not in schema.
+
+  await mongoose.connect(mongodb_uri, {
+    maxPoolSize: 50,
+    serverSelectionTimeoutMS: 5000,
+  });
+
+  console.log("MongoDB connected");
 }
 
-export const connectDB = async (): Promise<void> => {
-    
+export async function disconnectMongo() {
+  await mongoose.disconnect();
+  console.log("MongoDB disconnected");
 }
